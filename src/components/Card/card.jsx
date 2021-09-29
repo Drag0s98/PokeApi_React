53988d895250477355fac458f438a0562a483171
Drag0s98 <dragostanase1698@gmail.com>
2021-09-29 22:34:20 +0200

    first commit

---
+++ src/components/Card/card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <section>
+      <form action="">
+        <input type="text" />
+        <button>Search</button>
+      </form>
+    </section>
+  )
+};
+
+export default Card;
