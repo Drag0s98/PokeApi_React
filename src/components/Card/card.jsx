0b2d3cad90b8f2b793656a0bf56a5ad2f8691ddb
Drag0s98 <dragostanase1698@gmail.com>
2021-10-04 15:49:44 +0200

    style almost done

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,14 +1,30 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
+
+import './card.css'
+
 const Card = (props) => {
 
   // console.log(props.data[0].name);
-  console.log(props.data);
   return (
-    <section>
-      <p>
-        {props.data.name}
-      </p>
-    </section>
+    <>
+      <div className='card'>
+        <div className='cardHeader'>
+          <img src={props.data.sprites.front_default} alt={props.data.name} />
+          <h2 className='pokemonType'>Pokemon Type || {props.data.types[0].type.name}</h2>
+          <h4 className='pokemonName'>Pokemon || {props.data.name}</h4>
+        </div>
+        <div className='cardBody'>
+          <h4>Habilidad 1</h4>
+          <p>{props.data.abilities[0].ability.name}</p>
+          <h4>Habilidad 2</h4>
+          <p>{props.data.abilities[1].ability.name}</p>
+        </div>
+        <div className='cardFooter'>
+          <p>{props.data.id}</p>
+        </div>
+      </div>
+
+    </>
   )
 };
 
